68a0bebc09b21a35dd325f15628ec3408103e08c
Vedant Singh <ani.vedant@outlook.com>
2026-08-14 08:54:29 +0530

    feat: add activation function

--- perceptron.py
+++ perceptron.py
@@ -2,8 +2,12 @@ import numpy as np
 
 class Perceptron:
     def __init__(self, weights,bias):
-        self.weights=None
-        self.bias=None
+        self.weights=weights
+        self.bias=bias
 
     def pre_activation(self,x):
         return np.dot(self.weights,x)+self.bias
+
+    def activation(self,z):
+        return max(0,z)
+    
